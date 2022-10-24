4b2a47e75635adba37b4107af2d7f4175c5907d2
Ken <ken.aleksander@gmail.com>
2022-10-24 22:31:32 +0200

    :art: Stack-css

--- lunt-react/.storybook/main.js
+++ lunt-react/.storybook/main.js
@@ -1,16 +1,12 @@
-const path = require('path');
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.tsx"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
