8d17129636532e10bb82e5bc0b1a464aba093983
Ken <ken.aleksander@gmail.com>
2022-10-25 18:26:17 +0200

    :art: Stack-layout

--- lunt-react/src/Stack/Stack.stories.tsx
+++ lunt-react/src/Stack/Stack.stories.tsx
@@ -3,15 +3,59 @@ import type { Meta } from "@storybook/react";
 import { Stack } from "./index";
 
 export default {
+  title: "Layout/Stack",
   component: Stack,
+  argTypes: {
+    align: {
+      control: {
+        type: "radio",
+        options: ["start", "end", "center"],
+      },
+    },
+  },
 } as Meta<typeof Stack>;
 
-export const Default = () => {
-  return (
-    <Stack>
-      <span>Stack</span>
-      <span>Stack2</span>
-      <span>Stack3</span>
-    </Stack>
-  );
+const elements = (
+  <>
+    <span>Info</span>
+    <span>success</span>
+    <span>error</span>
+    <span>warning</span>
+  </>
+);
+
+export const Default = (props) => (
+  <div style={{ width: "10rem" }}>
+    <Stack {...props}>{elements}</Stack>
+  </div>
+);
+
+Default.args = {
+  spacing: "4",
+  fullWidth: false,
 };
+
+export const Spacing = () => (
+  <div className="rowgap">
+    <Stack spacing="1">{elements}</Stack>
+    <Stack spacing="2">{elements}</Stack>
+    <Stack spacing="3">{elements}</Stack>
+    <Stack spacing="4">{elements}</Stack>
+  </div>
+);
+
+export const align = () => (
+  <div className="rowgap">
+    <Stack align="start">{elements}</Stack>
+    <Stack align="center">{elements}</Stack>
+    <Stack align="end">{elements}</Stack>
+  </div>
+);
+
+export const fullWidth = () => (
+  <div style={{ width: "10rem" }}>
+    <Stack align="start" fullWidth>
+      {elements}
+    </Stack>
+  </div>
+);
