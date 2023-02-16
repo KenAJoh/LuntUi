35b992f646f72d7ea4173159fdc69dbfb83a1e66
Ken <ken.aleksander@gmail.com>
2023-02-16 23:01:08 +0100

    :fire: Fjernet gammel storybook kode

--- lunt-demo/src/LuntProvider/LuntProvider.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./lunt-provider.css";
-
-export interface LuntproviderProps {
-  children: React.ReactNode;
-}
-
-export const Luntprovider = ({ children }: LuntproviderProps) => <div>{children}</div>;
